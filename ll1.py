17f5e7addcf1fe1ee6451a3cd3f17117ded7bd6b
Parbhat Pandey <pandeypravat@gmail.com>
2021-09-01 09:42:10 +0545

    added try catch block in parse function

--- ll1.py
+++ ll1.py
@@ -31,7 +31,7 @@ def parse(expr, parse_table, terminals, non_terminals):
 
     orig_expr = expr+""
 
-    print("\t\t\tMatched\t\t\tInput\t\t\tAction\n")
+    print("\t\t\tMatched\t\t\tStack\t\t\tInput\t\t\tAction\n")
     print("\t\t\t-\t\t\t", end = "")
     for i in stack:
         print(i,  end = "")
@@ -46,41 +46,45 @@ def parse(expr, parse_table, terminals, non_terminals):
         #     stack=['$']
         # if(orig_expr == matched and len(expr) == 1 ):
         #     break
-        if(stack[0] == expr[0] and stack[0] == "$"):
-            flag=True
-            break
-        elif(stack[0] == expr[0]):
-            if(matched == "-"):
-                matched = expr[0]
-            else:    
-                matched = matched + expr[0]
-            action = "Matched "+expr[0]
-            expr = expr[1:]
-            stack.pop(0)
-
-        else:
-            action = parse_table[non_terminals.index(stack[0])][terminals.index(expr[0])]
-            stack.pop(0)            
-            i = 0
-            flag=False
-            if action=='Sync':
-                stack=['$']
+        try:
+            if(stack[0] == expr[0] and stack[0] == "$"):
                 flag=True
-            for item in action[2:]:
-                if flag:
-                    action='Matched $'
-                    break             #################DHYAN SE DHEKHIYE...... YEHI HE WHO SUSKS
-                if(item != "`"):
-                    stack.insert(i,item)
-                i+=1
+                break
+            elif(stack[0] == expr[0]):
+                if(matched == "-"):
+                    matched = expr[0]
+                else:    
+                    matched = matched + expr[0]
+                action = "Matched "+expr[0]
+                expr = expr[1:]
+                stack.pop(0)
+            else:
+                action = parse_table[non_terminals.index(stack[0])][terminals.index(expr[0])]
+                stack.pop(0)            
+                i = 0
+                flag=False
+                if action=='Sync':
+                    stack=['$']
+                    flag=True
+                for item in action[2:]:
+                    if flag:
+                        action='Matched $'
+                        break             #################DHYAN SE DHEKHIYE...... YEHI HE WHO SUSKS
+                    if(item != "`"):
+                        stack.insert(i,item)
+                    i+=1
 
-        print("\t\t\t"+matched+"\t\t\t", end = "")
-        for i in stack:
-            print(i,  end = "")
-        print("\t\t\t", end = "")
-        print(expr+"\t\t\t", end = "")
-        print(action)
+            print("\t\t\t"+matched+"\t\t\t", end = "")
+            for i in stack:
+                print(i,  end = "")
+            print("\t\t\t", end = "")
+            print(expr+"\t\t\t", end = "")
+            print(action)
+        except:
+            flag = False
+            break
 
-        if flag:
-            print("\n\nInput '{input}' is accepted".format(input=original[:-1]))
-
+    if flag:
+        print("\n\nInput '{input}' is accepted".format(input=original[:-1]))
+    else:
+        print("\n\nInput '{input}' is not accepted".format(input=original[:-1]))
